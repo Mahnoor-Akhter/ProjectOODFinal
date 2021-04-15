87810db318d0690ef2f57068bc0b5108b519b64d
Mahnoor-Akhter <60778404+Mahnoor-Akhter@users.noreply.github.com>
2021-04-15 11:02:21 +0100

    Add project files.

---
+++ Projectv2/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projectv2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		JewelleryDbEntities db = new JewelleryDbEntities();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			var query = from i in db.ItemTbls
+						select i;
+
+			var results = query.ToList();
+
+			//dgItems.ItemsSource = results;
+		}
+
+		//Describe what this does
+		private void btn2_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn1_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ItemsDG_CurrentCellChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnreset_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnsave_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnupdate_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btndelete_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void cbxcatrgy_Loaded(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void cbxtypes_Loaded(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void cbxcatrgy_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void btn4_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
